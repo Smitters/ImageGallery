c4fe3a41b82c8b6f2e049030d4d06fccb7bbc6a7
Smetankin Dmitry <smetankin93@gmail.com>
2021-06-15 11:06:26 +0300

    Initial Commit

---
+++ ImageGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Dmitry Smetankin on 15.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
