66661d5b1a304871248bb7f58345733ac22fd32f
Smetankin Dmitry <smetankin93@gmail.com>
2021-06-15 15:17:03 +0300

    Added token refresher

--- ImageGallery/Services/Networking/RequestRetrier.swift
+++ ImageGallery/Services/Networking/RequestRetrier.swift
@@ -8,4 +8,31 @@
 import Alamofire
 
 final class RequestRetrier: RequestInterceptor {
+    private lazy var networkService = Dependencies.current.services.networkService
+    private lazy var authorizationService = Dependencies.current.services.authorizationService
+    
+    func retry(_ request: Request,
+               for session: Session,
+               dueTo error: Error,
+               completion: @escaping (RetryResult) -> Void) {
+        guard let afError = error.asAFError else {
+            completion(.doNotRetryWithError(error))
+            return
+        }
+        
+        if afError.responseCode == 401 || afError.responseCode == 403 {
+            authorizationService.authorize { [weak self] result in
+                guard let self = self else { return }
+                switch result {
+                case .success(let response):
+                    self.networkService.update(sessionId: response.token)
+                    completion(.retry)
+                case .failure(let error):
+                    completion(.doNotRetryWithError(error))
+                }
+            }
+        } else {
+            completion(.doNotRetryWithError(error))
+        }
+    }
 }
